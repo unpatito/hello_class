759c466ee55e26eda52426a3f7438f2491ec384e
“Lucas <lutacocas@gmail.com>
2023-02-09 18:12:47 +0100

    Lucas

--- hello.py
+++ hello.py
@@ -4,8 +4,13 @@ def antonio():
 def samuel():
     print("Hola soy Samuel, compañero de DAW")
 
+    def lucas():
+        print("Hola soy Lucas, vuestro compañero de DAW.")
+
+
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
     samuel()
+    lucas()
     
