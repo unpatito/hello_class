b285a6905ba31344982f51d91d5a8756a6267e1e
Jose Zafrilla <jozaru27@gmail.com>
2023-02-09 18:11:44 +0100

    jose

--- hello.py
+++ hello.py
@@ -4,8 +4,12 @@ def antonio():
 def samuel():
     print("Hola soy Samuel, compañero de DAW")
 
+def jose():
+    print("Hola, soy Jose, barón de Turís, vuestro super compañero de DAW")
+
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
     samuel()
+    jose()
     
