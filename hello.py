9e25cc4921a9046608a9cd9a08069062141cf680
Jesus <jesusmarchantep@gmail.com>
2023-02-20 18:19:25 +0100

    Saludo de Jesus

--- hello.py
+++ hello.py
@@ -10,11 +10,21 @@ def jose():
 def jesus():
     print("Hola soy Jesus, compañero de DAW")
 
+<<<<<<< HEAD
+=======
+def jesus2():
+    print("Jesus os vuelve a saludar desde DAW")
+>>>>>>> 1a714c7 (Saludo de Jesus)
 
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
     samuel()
+<<<<<<< HEAD
     jose()
     jesus()
 
+=======
+    jesus()
+    jesus2()
+>>>>>>> 1a714c7 (Saludo de Jesus)
